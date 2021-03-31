21e070e4d2554721f492b401d99720739bacc5d5
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2021-03-31 12:48:41 +0500

    feat: implement base routes, file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import AppoRuter from './components/AppRouter';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppoRuter />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/AppRouter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+const AppoRuter = () => {
+
+  return (
+    <div> AppoRuter </div>
+  )
+}
+
+export default AppoRuter;
