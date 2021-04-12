83670fa35b991fd31ead144b7ef65b135af0a991
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2021-04-12 07:22:18 +0500

    feat: navigation, routing

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import { authRoutes, publicRoutes } from '../routes'
+import { SHOP_ROUTE } from '../utils/constants';
 
 const AppoRuter = () => {
+  const isAuth = false;
 
   return (
-    <div> AppoRuter </div>
+    <Switch>
+      {
+        isAuth && authRoutes.map(({ path, Component }) => 
+          <Route key={path} path={path} component={Component} exact />
+        )
+      }
+      {
+        publicRoutes.map(({ path, Component }) => 
+          <Route key={path} path={path} component={Component} exact />
+        )
+      }
+      <Redirect to={SHOP_ROUTE} />
+    </Switch>
   )
 }
 
